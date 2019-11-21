ed1fe14ac99749b90ce8678d363c0be103b7ab62
Samuel <samuelsoutullosobral@gmail.com>
2019-11-21 17:00:37 +0100

    #13: Implemented responsive camera

---
+++ Assets/Scripts/View/CameraAspectRatioRange.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class CameraAspectRatioRange : MonoBehaviour
+{
+    // Parameters
+    public float minAspect;
+    public float maxAspect;
+
+    // Components
+    private Camera camera;
+
+    void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        float aspect = (float)Screen.width / (float)Screen.height;
+
+        if (aspect <= minAspect)
+        {
+            Rect rect = camera.rect;
+            float scaleHeight = aspect / minAspect;
+
+            rect.width = 1f;
+            rect.height = scaleHeight;
+            rect.x = 0;
+            rect.y = (1f - scaleHeight) / 2f;
+
+            camera.rect = rect;
+        }
+        else if(aspect >= maxAspect)
+        {
+            float scalewidth = maxAspect / aspect;
+            Rect rect = camera.rect;
+
+            rect.width = scalewidth;
+            rect.height = 1f;
+            rect.x = (1f - scalewidth) / 2f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+        else
+        {
+            Rect rect = camera.rect;
+
+            rect.width = rect.height = 1;
+            rect.x = rect.y = 0;
+
+            camera.rect = rect;
+        }
+    }
+}
---
+++ Assets/Scripts/View/CameraHorizontalFov.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class CameraHorizontalFov : MonoBehaviour
+{
+    // Parameters
+    public float hFov;
+
+    // Components
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hFovRad = hFov * Mathf.Deg2Rad;
+        float camH = Mathf.Tan(hFovRad * 0.5f) / camera.aspect;
+        float vFovRad = Mathf.Atan(camH) * 2;
+
+        camera.fieldOfView = vFovRad * Mathf.Rad2Deg;
+    }
+}
