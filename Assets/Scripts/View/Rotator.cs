745bcf32ec2fe80bf661ff9e6f6455c411c647ce
Samuel <samuelsoutullosobral@gmail.com>
2019-11-21 17:48:43 +0100

    Added rotator to all planets

---
+++ Assets/Scripts/View/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(speed * Time.deltaTime * Vector3.down);
+    }
+}
