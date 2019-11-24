a01d6f34aa52a6e72dc7ce4c12a84f983372ce29
Samuel <samuelsoutullosobral@gmail.com>
2019-11-24 20:11:59 +0100

    Menu buttons implemented

---
+++ Assets/Scripts/UI/LevelButtonBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class LevelButtonBehaviour : MonoBehaviour, IPointerClickHandler
+{
+    // Parameters
+    public int levelIndex;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+}
