751f46a3a36ee8b6094bf7c3f684a5f939e77c17
Samuel <samuelsoutullosobral@gmail.com>
2019-11-27 21:12:20 +0100

    Added orbitator to small planet

---
+++ Assets/Scripts/Physics/Orbitator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbitator : MonoBehaviour
+{
+    // Parameters
+    public Transform orbitCenter;
+    public float orbitationVelocity;
+
+    // State
+    private float currentAngle;
+
+    // Initial values
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+
+    void Awake()
+    {
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = initialPosition;
+        transform.rotation = initialRotation;
+
+        transform.RotateAround(orbitCenter.position, Vector3.forward, currentAngle);
+
+        currentAngle += orbitationVelocity * Time.deltaTime;
+    }
+}
